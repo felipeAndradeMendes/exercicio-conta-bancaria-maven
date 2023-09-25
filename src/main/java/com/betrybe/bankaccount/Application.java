9c7d7014f7bdc4a7dbd091d42e661c5ec3615943
Felipe Mendes <felipemadruga@gmail.com>
2023-09-25 18:51:44 -0300

    feat(req 03): implementa classe Application

--- src/main/java/com/betrybe/bankaccount/Application.java
+++ src/main/java/com/betrybe/bankaccount/Application.java
@@ -1,5 +1,8 @@
 package com.betrybe.bankaccount;
 
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * A class.
  */
@@ -9,6 +12,18 @@ public class Application {
    * A simple get method.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Informe o número da conta:");
+    String account = scanner.nextLine();
+
+    if (!StringUtils.isNumeric(account)) {
+      System.out.println("Número da conta inválido!");
+    } else {
+      int accountNumeric = Integer.parseInt(account);
+      AccountNumberFormatter account1 = new AccountNumberFormatter();
+      String result = account1.formatAccountNumber(accountNumeric);
+      System.out.println("Número da conta: " + result);
+    }
 
   }
 

a3c4b385b0897b3f3bc07f378e10137b6e444b2e
Felipe Mendes <felipemadruga@gmail.com>
2023-11-09 11:53:24 -0300

    commit para subir

---
+++ src/main/java/com/betrybe/bankaccount/Principal.java
@@ -0,0 +1,8 @@
+package com.betrybe.bankaccount;
+
+public class Principal {
+  public static void main(String[] args) {
+    AccountNumberFormatter account1 = new AccountNumberFormatter();
+    account1.formatAccountNumber(334455);
+  }
+}
